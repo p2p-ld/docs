676b41da14993277fd5fd4559433f3118ea146a2
sneakers-the-rat <JLSaunders987@gmail.com>
2023-06-05 19:27:03 -0700

    building with mdbook as a library

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 // dummy file
 
+use mdbook::MDBook;
+
 fn main() {
-	println!("This package does nothing! It's just for gathering deps to build the book!")
+	let root_dir = "./";
+	let mut md = MDBook::load(root_dir)
+    	.expect("Unable to load the book");
+	md.build().expect("Building failed");
+
+
+
+	// println!("This package does nothing! It's just for gathering deps to build the book!")
+
+
 }
